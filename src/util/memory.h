3716b41cfc72cebeb796770b6e75fd47fa0375c6
cazzwastaken <cazzwastaken@gmail.com>
2022-02-22 18:48:34 +0200

    Renamed "_this" -> "vmt"

--- src/util/memory.h
+++ src/util/memory.h
@@ -5,15 +5,15 @@ namespace memory
 {
 	// call virtual function @ given index
 	template <typename Return, typename ... Arguments>
-	constexpr Return Call(void* _this, const std::uint32_t index, Arguments ... args) noexcept
+	constexpr Return Call(void* vmt, const std::uint32_t index, Arguments ... args) noexcept
 	{
 		using Function = Return(__thiscall*)(void*, decltype(args)...);
-		return (*static_cast<Function**>(_this))[index](_this, args...);
+		return (*static_cast<Function**>(vmt))[index](vmt, args...);
 	}
 
 	// get void pointer to virtual function @ given index
-	constexpr void* Get(void* _this, const std::uint32_t index) noexcept
+	constexpr void* Get(void* vmt, const std::uint32_t index) noexcept
 	{
-		return (*static_cast<void***>(_this))[index];
+		return (*static_cast<void***>(vmt))[index];
 	}
 }
