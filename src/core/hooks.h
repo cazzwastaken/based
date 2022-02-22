a44c121de89228fb145f29994b754c0d9b7d5cc2
cazzwastaken <cazzwastaken@gmail.com>
2022-02-22 16:31:48 +0200

    Added declarations for functions to be implemented

--- src/core/hooks.h
+++ src/core/hooks.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "interfaces.h"
 
 namespace hooks
 {
+	// call once to emplace all hooks
+	void Setup() noexcept;
 
+	// call once to restore all hooks
+	void Destroy() noexcept;
 }
--- src/core/interfaces.h
+++ src/core/interfaces.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "globals.h"
+
+#include "../valve/ibaseclientdll.h"
 
 namespace interfaces
 {
+	// call once to capture all interface pointers
+	void Setup() noexcept;
 
+	// capture an individual interface by name & module
+	template <typename Interface>
+	Interface* Capture(const char* moduleName, const char* interfaceName) noexcept;
+
+	// interface pointers
+	inline IBaseClientDLL* client = nullptr;
 }
