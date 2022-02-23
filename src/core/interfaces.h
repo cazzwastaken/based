dfbd3dac9e6f6a4fb2c3136e9e5572132e8a4496
cazzwastaken <cazzwastaken@gmail.com>
2022-02-23 21:18:00 +0200

    Added IMaterialSystem Interface

--- src/core/interfaces.h
+++ src/core/interfaces.h
@@ -5,6 +5,7 @@
 #include "../valve/icliententitylist.h"
 #include "../valve/iclientmodeshared.h"
 #include "../valve/iengineclient.h"
+#include "../valve/imaterialsystem.h"
 #include "../valve/istudiorender.h"
 
 // learn more about interfaces -> https://www.youtube.com/watch?v=C0wGdwnaArA
@@ -22,6 +23,7 @@ namespace interfaces
 	inline IClientEntityList* entityList = nullptr;
 	inline IClientModeShared* clientMode = nullptr;
 	inline IEngineClient* engine = nullptr;
+	inline IMaterialSystem* materialSystem = nullptr;
 	inline IStudioRender* studioRender = nullptr;
 
 	// other
---
+++ src/valve/imaterialsystem.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../util/memory.h"
+
+class CMaterial
+{
+public:
+	enum EMaterialVarFlag : std::int32_t
+	{
+		IGNOREZ = 1 << 15,
+		WIREFRAME = 1 << 28
+	};
+
+	constexpr void SetMaterialVarFlag(const std::int32_t flag, const bool on) noexcept
+	{
+		memory::Call<void>(this, 29, flag, on);
+	}
+};
+
+class IMaterialSystem
+{
+public:
+	constexpr CMaterial* FindMaterial(const char* name) noexcept
+	{
+		return memory::Call<CMaterial*>(this, 84, name, nullptr, true, nullptr);
+	}
+};
