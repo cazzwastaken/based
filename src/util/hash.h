9559e10833fc588176e6290b56232443d8381f80
cazzwastaken <cazzwastaken@gmail.com>
2022-02-23 18:25:03 +0200

    consistency

--- src/util/hash.h
+++ src/util/hash.h
@@ -4,13 +4,14 @@
 
 namespace hash
 {
+	// fnv constants
 	constexpr std::uint32_t BASE = 0x811C9DC5;
 	constexpr std::uint32_t PRIME = 0x1000193;
 
 	// compile-time FNV hash
 	consteval std::uint32_t CompileTime(const char* data, const std::uint32_t value = BASE) noexcept
 	{
-		return (data[0] == '\0') ? value : CompileTime(&data[1], (value ^ unsigned int(data[0])) * PRIME);
+		return (data[0] == '\0') ? value : CompileTime(&data[1], (value ^ std::uint32_t(data[0])) * PRIME);
 	}
 
 	// run-time FNV hash
@@ -18,6 +19,7 @@ namespace hash
 	{
 		std::uint32_t hashed = BASE;
 
+		// hash characters
 		for (const char& character : data)
 		{
 			hashed ^= character;
