566e2336f86150e879af4904de5a489ce573f59e
cazzwastaken <cazzwastaken@gmail.com>
2022-02-23 17:56:35 +0200

    Example BHOP in CreateMove hook & AllocKeyValues hook (unfinished) for retaddr bypass

--- src/core/hooks.cpp
+++ src/core/hooks.cpp
@@ -5,10 +5,61 @@
 
 void hooks::Setup() noexcept
 {
+	MH_Initialize();
 
+	// AllocKeyValuesMemory hook
+	MH_CreateHook(
+		memory::Get(interfaces::keyValuesSystem, 1),
+		&AllocKeyValuesMemory,
+		reinterpret_cast<void**>(&AllocKeyValuesMemoryOriginal)
+	);
+
+	// CreateMove hook
+	MH_CreateHook(
+		memory::Get(interfaces::clientMode, 24),
+		&CreateMove,
+		reinterpret_cast<void**>(&CreateMoveOriginal)
+	);
+
+	MH_EnableHook(MH_ALL_HOOKS);
 }
 
 void hooks::Destroy() noexcept
 {
+	// restore hooks
+	MH_DisableHook(MH_ALL_HOOKS);
+	MH_RemoveHook(MH_ALL_HOOKS);
 
+	// uninit minhook
+	MH_Uninitialize();
+}
+
+void* __stdcall hooks::AllocKeyValuesMemory(const std::int32_t size) noexcept
+{
+	// to do
+
+	return AllocKeyValuesMemoryOriginal(interfaces::keyValuesSystem, size);
+}
+
+bool __stdcall hooks::CreateMove(float frameTime, CUserCmd* cmd) noexcept
+{
+	// make sure this function is being called from CInput::CreateMove
+	if (!cmd->commandNumber)
+		return CreateMoveOriginal(interfaces::clientMode, frameTime, cmd);
+
+	// this would be done anyway by returning true
+	if (CreateMoveOriginal(interfaces::clientMode, frameTime, cmd))
+		interfaces::engine->SetViewAngles(cmd->viewAngles);
+
+	// set our local player
+	globals::localPlayer = interfaces::entityList->GetEntityFromIndex(interfaces::engine->GetLocalPlayerIndex());
+
+	if (globals::localPlayer && globals::localPlayer->IsAlive())
+	{
+		// example bhop
+		if (!(globals::localPlayer->GetFlags() & 1))
+			cmd->buttons &= ~CUserCmd::IN_JUMP;
+	}
+
+	return false;
 }
--- src/core/hooks.h
+++ src/core/hooks.h
@@ -8,4 +8,14 @@ namespace hooks
 
 	// call once to restore all hooks
 	void Destroy() noexcept;
+
+	// bypass return address checks (thx osiris)
+	using AllocKeyValuesMemoryFn = void* (__thiscall*)(void*, const std::int32_t) noexcept;
+	inline AllocKeyValuesMemoryFn AllocKeyValuesMemoryOriginal;
+	void* __stdcall AllocKeyValuesMemory(const std::int32_t size) noexcept;
+
+	// example CreateMove hook
+	using CreateMoveFn = bool(__thiscall*)(IClientModeShared*, float, CUserCmd*) noexcept;
+	inline CreateMoveFn CreateMoveOriginal = nullptr;
+	bool __stdcall CreateMove(float frameTime, CUserCmd* cmd) noexcept;
 }
