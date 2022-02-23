1210ae9e0186226266c79ff0c916274e0202f77b
cazzwastaken <cazzwastaken@gmail.com>
2022-02-23 17:55:11 +0200

    SetViewAngles Function

--- src/valve/iengineclient.h
+++ src/valve/iengineclient.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "../util/memory.h"
 
+#include "cvector.h"
+
 class IEngineClient
 {
 public:
 	constexpr std::int32_t GetLocalPlayerIndex() noexcept 
 	{
 		return memory::Call<std::int32_t>(this, 12);
+	}
+
+	constexpr void SetViewAngles(const CVector& viewAngles) noexcept 
+	{
+		memory::Call<void, const CVector&>(this, 19, viewAngles);
 	}
 
 	constexpr bool InGame() noexcept 
