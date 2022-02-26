fe02e5cccd323e26253ce63194a6215618cea81a
cazzwastaken <cazzwastaken@gmail.com>
2022-02-26 13:21:12 +0200

    Added enums for flags

--- src/valve/istudiorender.h
+++ src/valve/istudiorender.h
@@ -5,30 +5,61 @@ class CEntity;
 class CDrawModelInfo
 {
 public:
-	void* studio;
+	void* studioHdr;
 	void* hardwareData;
-	std::int32_t unused;
+	std::int32_t decals;
 	std::int32_t skin;
 	std::int32_t body;
 	std::int32_t hitboxSet;
 	CEntity* renderable;
 };
 
-class CMaterial;
+class IMaterial;
 class IStudioRender
 {
 public:
-	constexpr void ColorModulate(const float* color) noexcept
+	enum
+	{
+		STUDIORENDER_DRAW_ENTIRE_MODEL = 0,
+		STUDIORENDER_DRAW_OPAQUE_ONLY = 0x01,
+		STUDIORENDER_DRAW_TRANSLUCENT_ONLY = 0x02,
+		STUDIORENDER_DRAW_GROUP_MASK = 0x03,
+		STUDIORENDER_DRAW_NO_FLEXES = 0x04,
+		STUDIORENDER_DRAW_STATIC_LIGHTING = 0x08,
+		STUDIORENDER_DRAW_ACCURATETIME = 0x10,		// Use accurate timing when drawing the model.
+		STUDIORENDER_DRAW_NO_SHADOWS = 0x20,
+		STUDIORENDER_DRAW_GET_PERF_STATS = 0x40,
+		STUDIORENDER_DRAW_WIREFRAME = 0x80,
+		STUDIORENDER_DRAW_ITEM_BLINK = 0x100,
+		STUDIORENDER_SHADOWDEPTHTEXTURE = 0x200,
+		STUDIORENDER_UNUSED = 0x400,
+		STUDIORENDER_SKIP_DECALS = 0x800,
+		STUDIORENDER_MODEL_IS_CACHEABLE = 0x1000,
+		STUDIORENDER_SHADOWDEPTHTEXTURE_INCLUDE_TRANSLUCENT_MATERIALS = 0x2000,
+		STUDIORENDER_NO_PRIMARY_DRAW = 0x4000,
+		STUDIORENDER_SSAODEPTHTEXTURE = 0x8000,
+	};
+
+	enum EOverrideType : std::int32_t
+	{
+		OVERRIDE_NORMAL = 0,
+		OVERRIDE_BUILD_SHADOWS,
+		OVERRIDE_DEPTH_WRITE,
+		OVERRIDE_SELECTIVE,
+		OVERRIDE_SSAO_DEPTH_WRITE,
+	};
+
+	constexpr void SetColorModulation(const float* color) noexcept
 	{
 		memory::Call<void>(this, 27, color);
 	}
 
-	constexpr void AlphaModulate(const float alpha) noexcept
+	constexpr void SetAlphaModulation(const float alpha) noexcept
 	{
 		memory::Call<void>(this, 28, alpha);
 	}
 
-	constexpr void ForcedMaterialOverride(CMaterial* material, const std::int32_t type = 0, std::int32_t index = -1) noexcept
+	constexpr void ForcedMaterialOverride(IMaterial* material, const std::int32_t type = OVERRIDE_NORMAL, const std::int32_t index = -1) noexcept
 	{
 		memory::Call<void>(this, 33, material, type, index);
 	}
