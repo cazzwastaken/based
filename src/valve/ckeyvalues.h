aa6d020834cec18b2e01760ee78665d65f72f41f
cazzwastaken <cazzwastaken@gmail.com>
2022-03-17 19:53:22 +0200

    Implemented FromString function

--- src/valve/ckeyvalues.h
+++ src/valve/ckeyvalues.h
@@ -1,6 +1,26 @@
 #pragma once
+#include "../util/memory.h"
 
 class CKeyValues {
 public:
+	// https://gitlab.com/KittenPopo/csgo-2018-source/-/blob/main/tier1/keyvalues.cpp#L3652
+	static CKeyValues* FromString(const char* name, const char* value) noexcept
+	{
+		static std::uintptr_t functionAddress =
+			memory::RelativeToAbsolute(reinterpret_cast<std::uintptr_t>(memory::keyValuesFromString));
 
+		CKeyValues* kv;
+
+		__asm
+		{
+			push 0
+			mov edx, value			// second param goes in edx
+			mov ecx, name			// first param in ecx
+			call functionAddress	// call the func with ecx, edx
+			add esp, 4
+			mov kv, eax				// value is stored in eax, move it into kv
+		}
+
+		return kv;
+	}
 };
