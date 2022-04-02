9ef8403c9d8c7e8723e0838530a00e195e980ea6
cazzwastaken <cazzwastaken@gmail.com>
2022-04-02 18:08:09 +0200

    Added + & - CVector overloads

--- src/valve/cvector.h
+++ src/valve/cvector.h
@@ -4,5 +4,15 @@
 class CVector
 {
 public:
+	constexpr CVector operator+(const CVector& other) noexcept
+	{
+		return { x + other.x, y + other.y, z + other.z };
+	}
+
+	constexpr CVector operator-(const CVector& other) noexcept
+	{
+		return { x - other.x, y - other.y, z - other.z };
+	}
+
 	float x{ }, y{ }, z{ };
 };
