95e01191ac5804252c9f4637490d5023aae1efe2
cazzwastaken <cazzwastaken@gmail.com>
2022-04-02 15:31:31 +0200

    Type fix

--- src/valve/ivpanel.h
+++ src/valve/ivpanel.h
@@ -4,7 +4,7 @@
 class IVPanel
 {
 public:
-	constexpr const char* GetName(unsigned long panel) noexcept
+	constexpr const char* GetName(std::uint32_t panel) noexcept
 	{
 		return memory::Call<const char*>(this, 36, panel);
 	}
