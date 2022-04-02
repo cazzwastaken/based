e827ef5015239ec62bbcc520bfcc37b9f71c5826
cazzwastaken <cazzwastaken@gmail.com>
2022-04-02 14:56:14 +0200

    Added Origin() function to the matric class

--- src/valve/cmatrix.h
+++ src/valve/cmatrix.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#include "cvector.h"
+
 class CMatrix3x4
 {
 public:
+	constexpr float* operator[](size_t index) noexcept 
+	{
+		return data[index];
+	}
+
+	constexpr CVector Origin() noexcept 
+	{
+		return { data[0][3], data[1][3], data[2][3] };
+	}
+
 	float data[3][4];
 };
