7d691f68c58308d053b5b99cb2df6c29c01a2c46
cazzwastaken <cazzwastaken@gmail.com>
2022-05-19 17:28:16 +0200

    Added Render flags

--- src/valve/ispatialquery.h
+++ src/valve/ispatialquery.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <cstdint>
 
+// https://gitlab.com/KittenPopo/csgo-2018-source/-/blob/main/game/client/clientleafsystem.cpp#L223
+enum ERenderFlags
+{
+	RENDER_FLAGS_DISABLE_RENDERING = 0x01,
+	RENDER_FLAGS_HASCHANGED = 0x02,
+	RENDER_FLAGS_ALTERNATE_SORTING = 0x04,
+	RENDER_FLAGS_RENDER_WITH_VIEWMODELS = 0x08,
+	RENDER_FLAGS_BLOAT_BOUNDS = 0x10,
+	RENDER_FLAGS_BOUNDS_VALID = 0x20,
+	RENDER_FLAGS_BOUNDS_ALWAYS_RECOMPUTE = 0x40,
+	RENDER_FLAGS_IS_SPRITE = 0x80,
+	RENDER_FLAGS_FORCE_OPAQUE_PASS = 0x100,
+};
+
 // https://gitlab.com/KittenPopo/csgo-2018-source/-/blob/main/game/client/clientleafsystem.cpp#L240
 class IClientRenderable;
 class CRenderableInfo
