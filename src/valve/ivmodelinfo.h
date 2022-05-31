5f8ae0e3ecbfe01b11bf25ce59ff51f35d11b396
cazzwastaken <cazzwastaken@gmail.com>
2022-05-31 13:56:01 +0200

    Added hitboxes and stuff

--- src/valve/ivmodelinfo.h
+++ src/valve/ivmodelinfo.h
@@ -3,10 +3,112 @@
 
 #include "cvector.h"
 
-class CStudioHdr
+// thx qo0!
+enum EHitboxIndex : int
+{
+	HITBOX_INVALID = -1,
+	HITBOX_HEAD,
+	HITBOX_NECK,
+	HITBOX_PELVIS,
+	HITBOX_STOMACH,
+	HITBOX_THORAX,
+	HITBOX_CHEST,
+	HITBOX_UPPER_CHEST,
+	HITBOX_RIGHT_THIGH,
+	HITBOX_LEFT_THIGH,
+	HITBOX_RIGHT_CALF,
+	HITBOX_LEFT_CALF,
+	HITBOX_RIGHT_FOOT,
+	HITBOX_LEFT_FOOT,
+	HITBOX_RIGHT_HAND,
+	HITBOX_LEFT_HAND,
+	HITBOX_RIGHT_UPPER_ARM,
+	HITBOX_RIGHT_FOREARM,
+	HITBOX_LEFT_UPPER_ARM,
+	HITBOX_LEFT_FOREARM,
+	HITBOX_MAX
+};
+
+class CStudioBone
 {
 public:
 
+};
+
+class CStudioBoundingBox
+{
+public:
+	int bone;
+	int group;
+	CVector bbMin;
+	CVector bbMax;
+	int hitboxNameIndex;
+	CVector offsetRotation;
+	float capsuleRadius;
+	int unused[4];
+};
+
+class CStudioHitboxSet
+{
+public:
+	int nameIndex;
+	int numHitboxes;
+	int hitboxIndex;
+
+
+	inline const char* GetName() const noexcept 
+	{
+		return reinterpret_cast<char*>(std::uintptr_t(this) + nameIndex);
+	}
+
+	inline CStudioBoundingBox* GetHitbox(int index) noexcept
+	{
+		if (index < 0 || index >= numHitboxes)
+			return nullptr;
+
+		return reinterpret_cast<CStudioBoundingBox*>(reinterpret_cast<std::uint8_t*>(this) + hitboxIndex) + index;
+	}
+};
+
+class CStudioHdr
+{
+public:
+	inline CStudioBone* GetBone(int index) noexcept
+	{
+		if (index < 0 || index >= numBones)
+			return nullptr;
+
+		return reinterpret_cast<CStudioBone*>(std::uintptr_t(this) + boneIndex) + index;
+	}
+
+	inline CStudioHitboxSet* GetHitboxSet(int index) noexcept
+	{
+		if (index < 0 || index >= numHitboxSets)
+			return nullptr;
+
+		return reinterpret_cast<CStudioHitboxSet*>(std::uintptr_t(this) + hitboxSetIndex) + index;
+	}
+
+	int id;
+	int version;
+	int checksum;
+	char name[64];
+	int length;
+
+	CVector eyePosition;
+	CVector illumPosition;
+	CVector hullMin;
+	CVector hullMax;
+	CVector bbMin;
+	CVector bbMax;
+
+	int flags;
+	int numBones;
+	int boneIndex;
+	int numBoneControllers;
+	int boneControllerIndex;
+	int numHitboxSets;
+	int hitboxSetIndex;
 };
 
 class CModel
