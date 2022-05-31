cfade59a2a2431a3f0942fdb3c477b22048bcde1
cazzwastaken <cazzwastaken@gmail.com>
2022-05-31 13:39:23 +0200

    Added Scale() && ToAngle()

--- src/valve/cvector.h
+++ src/valve/cvector.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <numbers>
 #include <cstdint>
+#include <cmath>
 
 class CVector
 {
@@ -14,5 +16,40 @@ public:
 		return { x - other.x, y - other.y, z - other.z };
 	}
 
+	constexpr CVector operator*(const CVector& other) const noexcept
+	{
+		return { x * other.x, y * other.y, z * other.z };
+	}
+
+	constexpr CVector operator/(const CVector& other) const noexcept
+	{
+		return { x / other.x, y / other.y, z / other.z };
+	}
+
+	constexpr CVector Scale(float factor) const noexcept
+	{
+		return { x * factor, y * factor, z * factor };
+	}
+
+	inline CVector ToAngle() const noexcept
+	{
+		return {
+			std::atan2(-z, std::hypot(x, y)) * (180.0f / std::numbers::pi_v<float>),
+			std::atan2(y, x) * (180.0f / std::numbers::pi_v<float>),
+			0.0f 
+		};
+	}
+
 	float x{ }, y{ }, z{ };
+};
+
+__declspec(align(16)) class CVectorAligned : public CVector
+{
+public:
+	constexpr CVectorAligned operator-(const CVectorAligned& other) const noexcept
+	{
+		return { x - other.x, y - other.y, z - other.z, w - other.w };
+	}
+
+	float w{ };
 };
